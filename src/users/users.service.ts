fac8ed6b2d52df5e91bd3fcd6e291381c4914b27
MayerlyMunoz123 <mml.munoz@yavirac.edu.ec>
2024-06-10 21:17:38 -0500

    Descripción de los cambios

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './user.entity';
-;
+
 
 @Injectable()
 export class UsersService {
@@ -11,25 +11,47 @@ export class UsersService {
         private UserssRepository: Repository<Users>,
       ) {}
       async remove(id: number): Promise<void> {
-        await this.UserssRepository.delete(id);
+        const user = await this.encontrar1(id)
+        if(user) {
+          await this.UserssRepository.delete
+        }
       }
       async get(): Promise<any> {
-        return await this.UserssRepository.find();
+        const users= await this.UserssRepository.find();
+        if(!users || users.length == 0) {
+          throw new NotFoundException('no existen los registros')
+        }
+        return users
       }
       async update(id,data):Promise<any> {
-        return await this.UserssRepository.update(id,data);
-    }    
+        try {
+          await this.UserssRepository.update(id,data);
+        } catch (error) {
+          throw new BadRequestException("No se a podido actualizar el registro" + id )
+        }
+
+    }  
     async encontrar1(id): Promise<Users> {
-        return await this.UserssRepository.findOne({
+        const user = await this.UserssRepository.findOne({
           where:{
             id:id
           }
         }); 
+
+        if(!user) {
+          throw new NotFoundException('No existe el usuario' + id)
+        }
+
+        return user
       }
       async post(body): Promise<any> {
+        
+        try{  
         const newUser=await this.UserssRepository.create(body)
-        return await  this.UserssRepository.save(newUser); 
+        await  this.UserssRepository.save(newUser); 
+      
+      }catch(error){
+        throw new BadRequestException('No se a podido crear el registro del usuario', error)
       }
-
-}
-///prueba
+    }
+  }
